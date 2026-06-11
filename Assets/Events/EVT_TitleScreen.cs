57b54c9bb28c329afed63b35c129cc29c946362a
FikiFow Infinite Creation <fikifow08@gmail.com>
2026-06-12 05:35:55 +0700

    Intro Sequence

--- Assets/Events/EVT_TitleScreen.cs
+++ Assets/Events/EVT_TitleScreen.cs
@@ -1,12 +1,18 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Playables;
+using System.Collections;
+
 
 public class EVT_MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject NewGame_to_TitleSequence;
+    [SerializeField] private PlayableDirector TitleSequence;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        NewGame_to_TitleSequence.SetActive(false);
     }
 
     // Update is called once per frame
@@ -17,6 +23,16 @@ public class EVT_MainMenu : MonoBehaviour
 
     public void OnClick_NewGame()
     {
+        StartCoroutine(TitleSequence_Process());
+    }
+
+    IEnumerator TitleSequence_Process()
+    {
+        NewGame_to_TitleSequence.SetActive(true);
+        TitleSequence.Play();
+        Debug.Log("Title Sequence Started");
+        yield return new WaitForSeconds(58.5f);
+        Debug.Log("Title Sequence Finished");
         SceneManager.LoadScene("SampleScene");
     }
 
