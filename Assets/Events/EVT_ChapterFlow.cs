421abfa7fb36fe1f631b6321e342b378c620e82a
FikiFow Infinite Creation <fikifow08@gmail.com>
2026-06-12 22:10:37 +0700

    Adrian text 0

--- Assets/Events/EVT_ChapterFlow.cs
+++ Assets/Events/EVT_ChapterFlow.cs
@@ -13,6 +13,24 @@ public class EVT_ChapterFlow : MonoBehaviour
     IEnumerator atTheBeginning()
     {
         yield return StartCoroutine(FIKIFOW_GameOBJ_Transition.FadeOut(TransisiHitam, 0f, 120));
+        KIRISA_DialogueSystem.Instance.StartDialogCallback(
+            () => 
+            {          
+                Debug.Log("Dialog Selesai");
+            },
+            new KirisaDialogLine(
+                speaker: "ARIAN", 
+                dialog: "......", 
+                portrait: "", 
+                voice: ""
+            ),
+            new KirisaDialogLine(
+                speaker: "ADRIAN", 
+                dialog: "Akhirnya sampai juga di rumah. Kuy lah waktunya gadang ", 
+                portrait: "", 
+                voice: ""
+            )
+        );
     }
 
     // Update is called once per frame
